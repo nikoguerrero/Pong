87f2d5a3e36d510b7f5a8f293fe37405ad2ca39e
nikoguerrero <nclguerrero@gmail.com>
2021-06-25 20:01:11 -0400

    inicio pong'

---
+++ pongo.js
@@ -0,0 +1,8 @@
+console.log('hello world');
+
+let canvas = document.getElementById('canvas');
+
+let canvContext = canvas.getContext('2d');
+canvContext.fillRect(30, 10, 5, 30);
+canvContext.fillRect(265, 10, 5, 30);
+canvContext.fillRect(147.5, 72.5, 5, 5);
